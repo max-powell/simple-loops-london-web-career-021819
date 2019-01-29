abde47170e3d35ef916d6e58714155883fe856d4
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 11:57:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,8 +31,9 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
   while count < number_of_times
-    puts phrase
     count += 1
+    puts phrase
+    #count += 1
   end
 end
 
